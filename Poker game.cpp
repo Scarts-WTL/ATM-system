723e3dd9d40c58cf1fd961ee6f47e8f423104ec8
Scarts-WTL <arwin5267domino@gmail.com>
2025-07-24 21:07:07 +0800

    Add files via upload

---
+++ Poker game.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int balance = 0;
+    int amount, pin, num;
+
+    cout << "Welcome to ATM station";
+    cout << "\nEnter your pin number:  ";
+    cin >> pin;
+
+    if (pin == 1234) {
+
+        do {
+            cout << "\nChoose What service you want:\n";
+            cout << "1. Check Balance\n";
+            cout << "2. Deposit Money\n";
+            cout << "3. Withdraw Money\n";
+            cout << "4. Exit\n";
+			cout << "Enter your choice: ";
+            cin >> num;
+
+            if (num == 1) {
+                cout << "Your balance is: " << balance << endl;
+            }
+            else if (num == 2) {
+                cout << "Enter the amount you want to deposit: ";
+                cin >> amount;
+                balance += amount;
+                cout << "Your new balance is: " << balance << endl;
+            }
+            else if (num == 3) {
+                cout << "Enter the amount you want to withdraw: ";
+                cin >> amount;
+                if (amount > balance) {
+                    cout << "Insufficient balance!" << endl;
+                }
+                else {
+                    balance -= amount;
+                    cout << "Your new balance is: " << balance << endl;
+                }
+            }
+            else if (num == 4) {
+                cout << "Thank you for using our service!" << endl;
+            }
+            else {
+                cout << "Invalid choice!" << endl;
+            }
+        } while (num != 4);
+    }
+    else {
+        cout << "Incorrect pin number!" << endl;
+	}
+
+    return 0;
+}
